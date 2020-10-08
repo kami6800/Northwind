0d49cb002fad44407119fd6a10c4097378c9ceb2
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2020-10-08 10:47:03 +0200

    ddddddddddddddd ioseoifdjksdkjs

--- DataAccess/OrderRepository.cs
+++ DataAccess/OrderRepository.cs
@@ -14,6 +14,7 @@ namespace DataAccess
             return context.Invoices.ToList();
         }
 
+
         static public List<Invoices> GetInvoicesByCustomer(string customerId)
         {
             return context.Invoices.Where(o => o.CustomerId == customerId).ToList();
